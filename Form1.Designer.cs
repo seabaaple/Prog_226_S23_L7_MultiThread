95b5dffd9200f8a118977f15b52bcf1182500770
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-04-25 15:04:21 -0700

    In Class Notes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.rtbDisplay = new System.Windows.Forms.RichTextBox();
+            this.btnExample1 = new System.Windows.Forms.Button();
+            this.btnAsync = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbDisplay
+            // 
+            this.rtbDisplay.Location = new System.Drawing.Point(466, 72);
+            this.rtbDisplay.Name = "rtbDisplay";
+            this.rtbDisplay.Size = new System.Drawing.Size(444, 411);
+            this.rtbDisplay.TabIndex = 0;
+            this.rtbDisplay.Text = "";
+            // 
+            // btnExample1
+            // 
+            this.btnExample1.Location = new System.Drawing.Point(70, 116);
+            this.btnExample1.Name = "btnExample1";
+            this.btnExample1.Size = new System.Drawing.Size(238, 48);
+            this.btnExample1.TabIndex = 1;
+            this.btnExample1.Text = "Run ( Sync )";
+            this.btnExample1.UseVisualStyleBackColor = true;
+            this.btnExample1.Click += new System.EventHandler(this.btnExample1_Click);
+            // 
+            // btnAsync
+            // 
+            this.btnAsync.Location = new System.Drawing.Point(70, 221);
+            this.btnAsync.Name = "btnAsync";
+            this.btnAsync.Size = new System.Drawing.Size(238, 48);
+            this.btnAsync.TabIndex = 2;
+            this.btnAsync.Text = "Run ( ASync )";
+            this.btnAsync.UseVisualStyleBackColor = true;
+            this.btnAsync.Click += new System.EventHandler(this.btnAsync_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1016, 567);
+            this.Controls.Add(this.btnAsync);
+            this.Controls.Add(this.btnExample1);
+            this.Controls.Add(this.rtbDisplay);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox rtbDisplay;
+        private System.Windows.Forms.Button btnExample1;
+        private System.Windows.Forms.Button btnAsync;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Prog_226_S23_L7_MultiThread
 {
@@ -15,6 +17,66 @@ namespace Prog_226_S23_L7_MultiThread
         public Form1()
         {
             InitializeComponent();
+
+
+        } // Form1()
+
+        async void LongTask()
+        {
+            DisplayToRTB("LongTask Started");
+
+            await Task.Delay(4000);
+
+            DisplayToRTB("LongTask Ended");
+
         }
-    }
-}
+
+        void LongProcess()
+        {
+            DisplayToRTB("LongProcess Started");
+
+            //some code that takes long execution time 
+            Thread.Sleep(4000); // hold execution for 4 seconds
+
+            DisplayToRTB("LongProcessEnded");
+        } // LongProcess
+
+        void ShortProcess()
+        {
+            DisplayToRTB("ShortProcess Started");
+
+            //do something here
+
+            DisplayToRTB("ShortProcess Completed");
+        } // ShortProcess()
+
+        public void DisplayToRTB(string message)
+        {
+            rtbDisplay.Text += message;
+            rtbDisplay.Text += "\n";
+        } // DisplayToRTB
+
+        private void btnExample1_Click(object sender, EventArgs e)
+        {
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+
+            DisplayToRTB("Start of Example");
+            LongProcess();
+            ShortProcess();
+            DisplayToRTB("End of Example");
+
+            sw.Stop();
+
+            DisplayToRTB("-------------------" + sw.Elapsed.TotalSeconds.ToString());
+        }
+
+        private void btnAsync_Click(object sender, EventArgs e)
+        {
+            LongTask();
+            ShortProcess();
+        }
+    } // class
+
+} // namespace
